70986c945d7e280de2d4af390f1ab7f15f60f281
JamieX <102485831+IamJamieX@users.noreply.github.com>
2022-07-04 13:06:56 -0400

    Create AlwaysDay.lua

---
+++ AlwaysDay.lua
@@ -0,0 +1,6 @@
+local lighting = game:GetService("Lighting")
+while true do
+	lighting:SetMinutesAfterMidnight(660)
+	wait(0.000001)
+end
+print("ALWAYS DAY: ACTIVE! BIG thanks to JamieX for making this")
